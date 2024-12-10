65e3cc950fc8bad6ea8517cdd6abe9e4dc68bf5c
ciastek <jezowdominik@duck.com>
2024-12-10 13:10:09 +0100

    hashing code in database

--- testsite/myapp/models.py
+++ testsite/myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
 
 def upload_location(instance, filename):
     return 'labs/{}/{}'.format(instance.lab_type, filename)
@@ -16,7 +17,27 @@ class LabChoice(models.Model):
         return self.name
 
 class OneTimeCode(models.Model):
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='one_time_codes')
     used = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def set_code(self, raw_code):
+        """
+        Hash the code before saving
+        :param raw_code: Original unhashed code
+        """
+        self.code = make_password(raw_code)
+
+    def check_code(self, raw_code):
+        """
+        Verify if the provided code matches the stored hashed code
+        :param raw_code: Code to check
+        :return: Boolean indicating if code is correct
+        """
+        return check_password(raw_code, self.code)
+    
+    def save(self, *args, **kwargs):
+        if not self.code.startswith('pbkdf2_sha256$'):
+            self.set_code(self.code)
+        super().save(*args, **kwargs)
--- testsite/myapp/tests.py
+++ testsite/myapp/tests.py
@@ -1,4 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import OneTimeCode
 
-# Create your tests here.
+class OneTimeCodeTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.test_user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
 
+    def test_create_and_verify_code(self):
+        # Create a new one-time code
+        test_code = "abcde12345"
+        one_time_code = OneTimeCode.objects.create(
+            code=test_code,
+            user=self.test_user
+        )
+
+        # Check if code exists in database
+        self.assertTrue(OneTimeCode.objects.filter(user=self.test_user).exists())
+        
+        # Verify that the code was hashed and can be verified
+        retrieved_code = OneTimeCode.objects.get(user=self.test_user)
+        self.assertTrue(retrieved_code.check_code(test_code))
+
+    def tearDown(self):
+        # Clean up test data
+        self.test_user.delete()
+        
